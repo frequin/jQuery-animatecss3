b8d043842d19b4fe1a7e9cb3b72411a1fd75789a
Florent Requin <florent.requin@gmail.com>
2012-04-27 16:35:14 +0200

    no licence

--- jquery.animatecss3.js
+++ jquery.animatecss3.js
@@ -1,29 +1,8 @@
 /*
  * jQuery animatecss3 plugin
- * 
- * The MIT License
- * 
- * Copyright (c) 2011 Florent Requin
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * The software is provided "as is", without warranty of any kind, express or
- * implied, including but not limited to the warranties of merchantability,
- * fitness for a particular purpose and noninfringement. In no event shall the
- * authors or copyright holders be liable for any claim, damages or other
- * liability, whether in an action of contract, tort or otherwise, arising from,
- * out of or in connection with the software or the use or other dealings in
- * the software.
+ * Florent Requin - https://github.com/frequin/jQuery-animatecss3
+ * No licence - Feel free to use it as you like
  */
-
 
 /*
  * The jQuery animatecss3 plugin, like the jQuery animate method, performs a
